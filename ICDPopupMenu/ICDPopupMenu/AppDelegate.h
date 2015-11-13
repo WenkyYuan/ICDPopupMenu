c45d13d4dddf65870ed84a8df6a5d68c7868b7be
wenky <yuanwenji@iorcas.com>
2015-11-13 14:19:00 +0800

    新建项目

---
+++ ICDPopupMenu/ICDPopupMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ICDPopupMenu
+//
+//  Created by wenky on 15/11/13.
+//  Copyright (c) 2015年 wenky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ICDPopupMenu/ICDPopupMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ICDPopupMenu
+//
+//  Created by wenky on 15/11/13.
+//  Copyright (c) 2015年 wenky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
