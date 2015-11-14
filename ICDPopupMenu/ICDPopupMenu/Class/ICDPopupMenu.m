39b6b0a21132c5d1154b5a61338040baaa394507
wenky <yuanwenji@iorcas.com>
2015-11-14 11:33:49 +0800

    将ICDPopup整合进ICDPopupMenu

--- ICDPopupMenu/ICDPopupMenu/Class/ICDPopupMenu.m
+++ ICDPopupMenu/ICDPopupMenu/Class/ICDPopupMenu.m
@@ -7,8 +7,158 @@
 //
 
 #import "ICDPopupMenu.h"
-#import "ICDPopup.h"
 #import "UIImage+Color.h"
+
+@interface ICDPopup : UIView
+
++ (instancetype)popupWithContentView:(UIView *)contentView;
+
+- (void)showAtCenter:(CGPoint)center startPoint:(CGPoint)startPoin inView:(UIView *)inView animation:(BOOL)animation;
+
+- (void)dismiss:(BOOL)animation;
+
+@end
+
+@interface ICDPopup ()
+@property (nonatomic, strong) UIView *containerView;
+@property (nonatomic, strong) UIView *contentView;
+@property (nonatomic, assign) CGPoint containerStartPoint;
+@property (nonatomic, assign) BOOL dismissing;
+
+@end
+@implementation ICDPopup
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        _containerView = [[UIView alloc] init];
+        _containerView.backgroundColor = [UIColor clearColor];
+        _containerView.userInteractionEnabled = YES;
+        [self addSubview:_containerView];
+    }
+    return self;
+}
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    UIView* hitView = [super hitTest:point withEvent:event];
+    
+    if (hitView == self) {
+        [self dismiss:YES];
+    }
+    return hitView;
+}
+
++ (instancetype)popupWithContentView:(UIView *)contentView {
+    ICDPopup *popup = [[[self class] alloc] init];
+    popup.contentView = contentView;
+    return popup;
+}
+
+- (void)showAtCenter:(CGPoint)center startPoint:(CGPoint)startPoint inView:(UIView *)inView animation:(BOOL)animation{
+    if(!self.superview){
+        NSEnumerator *frontToBackWindows = [[[UIApplication sharedApplication] windows] reverseObjectEnumerator];
+        
+        for (UIWindow *window in frontToBackWindows) {
+            if (window.windowLevel == UIWindowLevelNormal) {
+                [window addSubview:self];
+                break;
+            }
+        }
+    }
+    self.frame = self.window.bounds;
+    
+    if (self.contentView.superview) {
+        [self.contentView removeFromSuperview];
+    }
+    [self.containerView addSubview:self.contentView];
+    
+    CGFloat width = CGRectGetWidth(self.contentView.frame);
+    CGFloat height = CGRectGetHeight(self.contentView.frame);
+    self.contentView.frame = CGRectMake(0, 0, width, height);
+    
+    CGPoint containerCenter = [self convertPoint:center fromView:inView];
+    CGPoint containerStartPoint = [self convertPoint:startPoint fromView:inView];
+    self.containerStartPoint = containerStartPoint;
+    
+    self.containerView.frame = CGRectMake(containerCenter.x - width / 2, containerCenter.y - height / 2, width, height);
+    
+    if (animation) {
+        self.containerView.alpha = 0.0;
+        CGRect finalContainerFrame = self.containerView.frame;
+        self.containerView.transform = CGAffineTransformMakeScale(0.01, 0.01);
+        self.containerView.center = containerStartPoint;
+        [UIView animateWithDuration:0.6
+                              delay:0.0
+             usingSpringWithDamping:0.8
+              initialSpringVelocity:15.0
+                            options:0
+                         animations:^{
+                             self.containerView.alpha = 1.0;
+                             self.containerView.transform = CGAffineTransformIdentity;
+                             self.containerView.frame = finalContainerFrame;
+                         }
+                         completion:nil];
+    }
+}
+
+- (void)dismiss:(BOOL)animation {
+    if (self.dismissing) {
+        return;
+    }
+    self.dismissing = YES;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        void (^completionBlock)(BOOL) = ^(BOOL finished) {
+            if (finished) {
+                self.dismissing = NO;
+                [self removeFromSuperview];
+            }
+        };
+        
+        CGPoint finalContainerCenter = self.containerStartPoint;
+        if (animation) {
+            [UIView animateWithDuration:0.3
+                                  delay:0
+                                options:UIViewAnimationOptionCurveEaseIn
+                             animations:^(void){
+                                 self.containerView.alpha = 0.0;
+                                 self.containerView.transform = CGAffineTransformMakeScale(0.01, 0.01);
+                                 self.containerView.center = finalContainerCenter;
+                             }
+                             completion:completionBlock];
+        } else {
+            self.containerView.alpha = 0.0;
+            completionBlock(YES);
+        }
+    });
+}
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@interface UIView (ICDPopup)
+
+- (void)dismissPresentingICDPopup;
+
+@end
+
+@implementation UIView (ICDPopup)
+
+- (void)dismissPresentingICDPopup {
+    // Iterate over superviews until you find a ICDPopup and dismiss it, then gtfo
+    UIView* view = self;
+    while (view != nil) {
+        if ([view isKindOfClass:[ICDPopup class]]) {
+            [(ICDPopup*)view dismiss:NO];
+            break;
+        }
+        view = [view superview];
+    }
+}
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static const CGFloat kArrowWidth = 12;
 static const CGFloat kArrowHeight = 8;
@@ -166,13 +316,32 @@ static const NSUInteger kButtonTagOffset = 1000;
     [self addArrowLayer:startPoint inView:inView position:position];
 }
 
+#pragma mark - private methods
+- (void)setup {
+    self.backgroundColor = [UIColor clearColor];
+    
+    _arrowLayerBgView = [[UIView alloc] init];
+    _arrowLayerBgView.backgroundColor = [UIColor clearColor];
+    [self addSubview:_arrowLayerBgView];
+    
+    _itemContainerView = [[UIView alloc] init];
+    _itemContainerView.backgroundColor = [UIColor clearColor];
+    _itemContainerView.layer.cornerRadius = 5;
+    _itemContainerView.layer.masksToBounds = YES;
+    [self addSubview:_itemContainerView];
+}
+
+- (void)hide {
+    [self dismissPresentingICDPopup];
+}
+
 - (void)addArrowLayer:(CGPoint)startPoint inView:(UIView *)inView position:(ICDPopupMenuArrowPosition)position {
     startPoint = [self convertPoint:startPoint fromView:inView];
     
     CGPoint arrowTopPoint = startPoint;
     CGPoint arrowPointA = CGPointMake(arrowTopPoint.x - kArrowWidth/2, kArrowHeight);
     CGPoint arrowPointB = CGPointMake(arrowTopPoint.x + kArrowWidth/2, kArrowHeight);
-
+    
     if (position == ICDPopupMenuArrowPositionTopLeft || position == ICDPopupMenuArrowPositionTopCenter || position == ICDPopupMenuArrowPositionTopRight) {
         arrowPointA = CGPointMake(arrowTopPoint.x - kArrowWidth/2, arrowTopPoint.y + kArrowHeight);
         arrowPointB = CGPointMake(arrowTopPoint.x + kArrowWidth/2, arrowTopPoint.y + kArrowHeight);
@@ -197,31 +366,13 @@ static const NSUInteger kButtonTagOffset = 1000;
     [_arrowLayerBgView.layer addSublayer:layer];
 }
 
-#pragma mark - private methods
-- (void)setup {
-    self.backgroundColor = [UIColor clearColor];
-    
-    _arrowLayerBgView = [[UIView alloc] init];
-    _arrowLayerBgView.backgroundColor = [UIColor clearColor];
-    [self addSubview:_arrowLayerBgView];
-    
-    _itemContainerView = [[UIView alloc] init];
-    _itemContainerView.backgroundColor = [UIColor clearColor];
-    _itemContainerView.layer.cornerRadius = 5;
-    _itemContainerView.layer.masksToBounds = YES;
-    [self addSubview:_itemContainerView];
-}
-
-- (void)hide {
-    [self dismissPresentingICDPopup];
-}
-
 - (UIButton *)createItemWithTitle:(NSString *)title imageName:(NSString *)imageName tag:(NSInteger)tag {
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     [button setTitle:title forState:UIControlStateNormal];
     [button setImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
+    [button setImage:[UIImage imageNamed:imageName] forState:UIControlStateHighlighted];
     [button setBackgroundColor:[UIColor clearColor]];
-    [button setBackgroundImage:[UIImage imageWithColor:UIColorFromRGBA(0x000000, 0.3)] forState:UIControlStateHighlighted];
+    [button setBackgroundImage:[UIImage imageWithColor:UIColorFromRGBA(0x000000, 0.9)] forState:UIControlStateHighlighted];
     [button addTarget:self action:@selector(didTapButton:) forControlEvents:UIControlEventTouchUpInside];
     [button setImageEdgeInsets:UIEdgeInsetsMake(0, 16, 0, 0)];
     [button setTitleEdgeInsets:UIEdgeInsetsMake(0, 24, 0, 0)];
@@ -266,6 +417,8 @@ static const NSUInteger kButtonTagOffset = 1000;
 }
 
 @end
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 @implementation ICDPopupMenuItem
 
